79c1ba8f41802869db060dc41a58db2401d755a0
MichaelDoro <Michael.doroshenko@codere.com>
2023-06-07 10:39:13 +0300

    added hreflangs for alternate links

--- src/Components/pages/splash/splash.jsx
+++ src/Components/pages/splash/splash.jsx
@@ -25,7 +25,17 @@ const Splash = (props) => {
           name="description"
           content="La casa de apuestas deportivas y casino online #1 en Colombia. Juega en línea Blackjack, ruleta, slots y apuesta en tus deportes favoritos ⚽."
         ></meta>
-
+        <link rel="alternate" hreflang="x-default" href="https://www.codereonline.com/overview/default.aspx"/>
+        <link rel="alternate" hreflang="es-es" href="https://www.codere.es/"/>
+        <link rel="alternate" hreflang="en-es" href="https://www.codere.es/"/>
+        <link rel="alternate" hreflang="es-mx" href="https://www.codere.mx/"/>
+        <link rel="alternate" hreflang="en-mx" href="https://www.codere.mx/"/>
+        <link rel="alternate" hreflang="es-pa" href="https://www.codere.pa/"/>
+        <link rel="alternate" hreflang="en-pa" href="https://www.codere.pa/"/>
+        <link rel="alternate" hreflang="es-co" href="https://www.codere.com.co/"/>
+        <link rel="alternate" hreflang="en-co" href="https://www.codere.com.co/"/>
+        <link rel="alternate" hreflang="es-ar" href="https://www.codere.bet.ar/"/>
+        <link rel="alternate" hreflang="en-ar" href="https://www.codere.bet.ar/"/>
 
       </Helmet>
 
